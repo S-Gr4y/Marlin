f4f6615f9de882f19bdbe3dbcb7b613d4ba67765
Qaz <quentin.amoudruz@gmail.com>
2017-04-17 17:16:04 +0200

    Enabled manual mesh bed leveling.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -552,7 +552,7 @@
  *   Use G29 repeatedly, adjusting the Z height at each point with movement commands
  *   or (with LCD_BED_LEVELING) the LCD controller.
  */
-//#define PROBE_MANUALLY
+#define PROBE_MANUALLY
 
 /**
  *   A Fix-Mounted Probe either doesn't deploy or needs manual deployment.
@@ -847,7 +847,7 @@
 //#define AUTO_BED_LEVELING_LINEAR
 //#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
-//#define MESH_BED_LEVELING
+#define MESH_BED_LEVELING
 
 /**
  * Enable detailed logging of G28, G29, M48, etc.
@@ -941,11 +941,11 @@
  * Use the LCD controller for bed leveling
  * Requires MESH_BED_LEVELING or PROBE_MANUALLY
  */
-//#define LCD_BED_LEVELING
+#define LCD_BED_LEVELING
 
 #if ENABLED(LCD_BED_LEVELING)
   #define MBL_Z_STEP 0.025    // Step size while manually probing Z axis.
-  #define LCD_PROBE_Z_RANGE 4 // Z Range centered on Z_MIN_POS for LCD Z adjustment
+  #define LCD_PROBE_Z_RANGE 5 // Z Range centered on Z_MIN_POS for LCD Z adjustment
 #endif
 
 /**
